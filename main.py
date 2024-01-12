9dd3f6f7479450a2faa9e7f4d58f0fc62e2ba1a4
AnCapone <okai.kot44@gmail.com>
2024-01-12 11:18:37 +0200

    Implement first task

---
+++ main.py
@@ -0,0 +1,10 @@
+# домашній номер телефону (тільки цифри та довжина номера)
+import re
+
+while True:
+    homePhoneNumber = re.fullmatch(r'\d{7}', input("Введіть номер телефону без коду (7 цифр без пробілів): "))
+    if homePhoneNumber is None:
+        print("Невірний формат вводу!")
+        continue
+    print("Введення вірне!")
+    break
